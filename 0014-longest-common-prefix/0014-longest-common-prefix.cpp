d3565fb0b98e7540c6bf9fc717bf2f3cfe9c4acc
Aryanv1118 <aryanv1118.work@gmail.com>
2026-08-05 18:02:29 +0530

    Time: 0 ms (100%), Space: 11.8 MB (79.21%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,51 +1,19 @@
-class TrieNode{
-    public:
-        vector<TrieNode*> next;
-        bool end;
-        TrieNode(){
-            next.assign(26,nullptr);
-            end = false;
-        };
-};
 class Solution {
 public:
-    int ans = 0;
-    void insert(string& s,TrieNode* root){
-        TrieNode* curr = root;
-        for(char c:s){
-            if(curr->next[c-'a'] == nullptr)
-                curr->next[c-'a'] = new TrieNode();
-            curr = curr->next[c-'a'];
-        }
-        curr->end = true;
-    }
-    int chcount(TrieNode* root,int &idx){
-        int count = 0;
-        idx = -1;
-        for(int i = 0;i<26;i++){
-            if(root->next[i]!=nullptr){
-                count++;
-                idx = i;
-            }
-        }
-        return count;
-    }
     string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
-        TrieNode* root = new TrieNode();
-        for(auto &s:strs){
-            insert(s,root);
+    string ans = "";
+
+    for (int i = 0; i < strs[0].size(); i++) {
+        char ch = strs[0][i];
+
+        for (int j = 1; j < strs.size(); j++) {
+            if (i >= strs[j].size() || strs[j][i] != ch)
+                return ans;
         }
-        TrieNode* temp = root;
-        while(true){
-            int idx;
-            int count = chcount(temp,idx);
-            if(count!=1 || temp->end){
-                break;
-            }
-            ans += 'a'+idx;
-            temp = temp->next[idx];
-        }
-        return ans;
+
+        ans += ch;
     }
+
+    return ans;
+}
 };
