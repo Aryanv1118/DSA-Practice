bce2bf822cd135f34daa95fc3651e52c234e36be
Aryanv1118 <aryanv1118.work@gmail.com>
2026-08-19 13:52:28 +0530

    Time: 543 ms (5%), Space: 40.3 MB (19.03%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<=2) return 0;
+        vector<bool>sieve(n,true);
+        sieve[0] = sieve[1] = false;
+        int ans = n/2;
+        for (int i = 3; i * i < n; i += 2) {
+            if (sieve[i]) {
+                for (int j = i * i; j < n; j += 2 * i) {
+                    if (sieve[j]) {
+                        sieve[j] = false;
+                        ans--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
