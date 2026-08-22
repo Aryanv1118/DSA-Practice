ab1c43b93bebbcfc099548a64cfd57001bf95c65
Aryanv1118 <aryanv1118.work@gmail.com>
2026-08-23 02:07:49 +0530

    Time: 0 ms (100%), Space: 10.5 MB (66.18%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles){
-        return true;
+    bool stoneGame(vector<int>& piles) {
+        int total = 0;
+        for(int x:piles){
+            total ^= x;
+        }
+        return total%2 == 1;
     }
 };
